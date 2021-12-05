aabac202c987706bfeed0fb81a1e53bb907b07b8
UiWebKit <uniwebkit@gmail.com>
2021-12-05 21:19:07 +0200

    [2.0.0-8] remove slot for uni-display

--- dist/p-5ab113f9.entry.js
+++
@@ -1,1 +0,0 @@
-export{U as uni_display}from"./p-d4e05b75.js";import"./p-165e1107.js";import"./p-0aa8ab41.js";import"./p-12df985d.js";import"./p-e282851d.js";import"./p-94678ab8.js";
---
+++ dist/p-ac7c26cc.entry.js
@@ -0,0 +1,1 @@
+export{U as uni_display}from"./p-6240f6e0.js";import"./p-165e1107.js";import"./p-0aa8ab41.js";import"./p-12df985d.js";import"./p-e282851d.js";import"./p-94678ab8.js";
